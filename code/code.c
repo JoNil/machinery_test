867d50118e031d36e16a9e8ff2ca19efed1c757c
Jonathan Nilsson <jonathan@voysys.se>
2020-11-21 23:22:15 +0100

    blinky light

--- code/code.c
+++ code/code.c
@@ -1,21 +1,29 @@
 static struct tm_entity_api *tm_entity_api;
-static struct tm_link_component_api *tm_link_component_api;
-static struct tm_temp_allocator_api *tm_temp_allocator_api;
 static struct tm_the_truth_api *tm_the_truth_api;
+static struct tm_graph_interpreter_api *tm_graph_interpreter_api;
+static struct tm_graph_component_api *tm_graph_component_api;
+static struct tm_logger_api *tm_logger_api;
+
+#include <stdlib.h>
 
 #include <plugins/the_machinery_shared/component_interfaces/editor_ui_interface.h>
 
 #include <plugins/entity/entity.h>
-#include <plugins/entity/link_component.h>
-#include <plugins/entity/transform_component.h>
+#include <plugins/graph_interpreter/graph_component.h>
+#include <plugins/graph_interpreter/graph_interpreter.h>
+#include <plugins/default_render_pipe/light_component.h>
 
 #include <foundation/api_registry.h>
-#include <foundation/carray.inl>
+#include <foundation/color_spaces.inl>
+#include <foundation/log.h>
+#include <foundation/macros.h>
 #include <foundation/math.inl>
 #include <foundation/the_truth.h>
 
 #define TM_TT_TYPE__CAVE_COMPONENT "tm_cave_component"
 #define TM_TT_TYPE_HASH__CAVE_COMPONENT TM_STATIC_HASH("tm_cave_component", 0x777acd9d836b08a6ULL)
+
+#define GRAPH_DIST_HASH TM_STATIC_HASH("Dist", 0xd7882eed6b5209abULL)
 
 enum
 {
@@ -70,14 +78,7 @@ static void component__create(struct tm_entity_context_o *ctx)
 // Runs on (cave_component, transform_component, link_component)
 static void engine_update__cave_component(tm_engine_o *inst, tm_engine_update_set_t *data)
 {
-    TM_INIT_TEMP_ALLOCATOR(ta);
-
-    tm_entity_t *mod_link = 0;
-    tm_entity_t *mod_transform = 0;
-
-    struct tm_entity_context_o *ctx = (struct tm_entity_context_o *)inst;
-    const uint32_t link_component = tm_entity_api->lookup_component(ctx, TM_TT_TYPE_HASH__LINK_COMPONENT);
-    void *link_manager = tm_entity_api->component(ctx, link_component)->manager;
+    //struct tm_entity_context_o *ctx = (struct tm_entity_context_o *)inst;
 
     double t = 0;
     for (const tm_entity_blackboard_value_t *bb = data->blackboard_start; bb != data->blackboard_end; ++bb)
@@ -88,54 +89,44 @@ static void engine_update__cave_component(tm_engine_o *inst, tm_engine_update_se
 
     for (tm_engine_update_array_t *a = data->arrays; a < data->arrays + data->num_arrays; ++a)
     {
-        struct tm_cave_component_t *cave_component = a->components[0];
-        tm_transform_component_t *transform = a->components[1];
-        tm_link_component_t *link = a->components[2];
+        struct tm_light_component_t *light_component = a->components[1];
+        struct tm_graph_component_t *graph_component = a->components[2];
 
         for (uint32_t i = 0; i < a->n; ++i)
         {
-            if (!cave_component[i].y0)
-                cave_component[i].y0 = transform[i].tm.pos.y;
-            const float y = cave_component[i].y0 + cave_component[i].amplitude * sinf((float)t * cave_component[i].frequency);
+            tm_graph_interpreter_wire_content_t dist_wire = tm_graph_interpreter_api->read_variable(graph_component->gr, GRAPH_DIST_HASH);
 
-            if (link)
+            if (dist_wire.data != NULL)
             {
-                link[i].local_transform.pos.y = y;
-                tm_carray_temp_push(mod_link, a->entities[i], ta);
-            }
-            else
-            {
-                transform[i].tm.pos.y = y;
-                ++transform[i].version;
-                tm_carray_temp_push(mod_transform, a->entities[i], ta);
+                float distance_to_wall = *(float *)dist_wire.data;
+                float hue = (sinf(0.4f * distance_to_wall) + 1.0f) / 2.0f;
+                TM_LOG("HUE: %f", hue);
+                light_component->color_rgb = tm_hue_to_rgb(hue);
             }
         }
     }
 
-    for (tm_entity_t *e = mod_link; e != tm_carray_end(mod_link); ++e)
-        tm_link_component_api->transform(link_manager, *e);
-
-    tm_entity_api->notify(ctx, data->engine->components[1], mod_transform, (uint32_t)tm_carray_size(mod_transform));
-
-    TM_SHUTDOWN_TEMP_ALLOCATOR(ta);
+    //tm_entity_api->notify(ctx, data->engine->components[1], mod_transform, (uint32_t)tm_carray_size(mod_transform));
 }
 
 static bool engine_filter__cave_component(tm_engine_o *inst, const uint32_t *components, uint32_t num_components, const tm_component_mask_t *mask)
 {
-    return tm_entity_mask_has_component(mask, components[0]) && (tm_entity_mask_has_component(mask, components[1]) || tm_entity_mask_has_component(mask, components[2]));
+    return tm_entity_mask_has_component(mask, components[0]) &&
+           tm_entity_mask_has_component(mask, components[1]) &&
+           tm_entity_mask_has_component(mask, components[2]);
 }
 
 static void component__register_engine(struct tm_entity_context_o *ctx)
 {
     const uint32_t cave_component = tm_entity_api->lookup_component(ctx, TM_TT_TYPE_HASH__CAVE_COMPONENT);
-    const uint32_t transform_component = tm_entity_api->lookup_component(ctx, TM_TT_TYPE_HASH__TRANSFORM_COMPONENT);
-    const uint32_t link_component = tm_entity_api->lookup_component(ctx, TM_TT_TYPE_HASH__LINK_COMPONENT);
+    const uint32_t light_component = tm_entity_api->lookup_component(ctx, TM_TT_TYPE_HASH__LIGHT_COMPONENT);
+    const uint32_t graph_component = tm_entity_api->lookup_component(ctx, TM_TT_TYPE_HASH__GRAPH_COMPONENT);
 
     const tm_engine_i cave_component_engine = {
         .name = "Cave Component",
         .num_components = 3,
-        .components = {cave_component, transform_component, link_component},
-        .writes = {false, true, true},
+        .components = {cave_component, light_component, graph_component},
+        .writes = {false, true, false},
         .update = engine_update__cave_component,
         .filter = engine_filter__cave_component,
         .inst = (tm_engine_o *)ctx,
@@ -146,9 +137,12 @@ static void component__register_engine(struct tm_entity_context_o *ctx)
 TM_DLL_EXPORT void tm_load_plugin(struct tm_api_registry_api *reg, bool load)
 {
     tm_entity_api = reg->get(TM_ENTITY_API_NAME);
-    tm_link_component_api = reg->get(TM_LINK_COMPONENT_API_NAME);
     tm_the_truth_api = reg->get(TM_THE_TRUTH_API_NAME);
-    tm_temp_allocator_api = reg->get(TM_TEMP_ALLOCATOR_API_NAME);
+    tm_graph_interpreter_api = reg->get(TM_GRAPH_INTERPRETER_API_NAME);
+    tm_graph_component_api = reg->get(TM_GRAPH_COMPONENT_API_NAME);
+    tm_logger_api = reg->get(TM_LOGGER_API_NAME);
+
+    TM_LOG("LOAD");
 
     tm_add_or_remove_implementation(reg, load, TM_THE_TRUTH_CREATE_TYPES_INTERFACE_NAME, truth__create_types);
     tm_add_or_remove_implementation(reg, load, TM_ENTITY_CREATE_COMPONENT_INTERFACE_NAME, component__create);
