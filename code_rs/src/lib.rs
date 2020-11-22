2969b52e99a593c96bbd4fbcd8de38fabd2226da
Jonathan Nilsson <jonathan@voysys.se>
2020-11-23 00:21:50 +0100

    Rusty log api

--- code_rs/src/lib.rs
+++ code_rs/src/lib.rs
@@ -1,22 +1,12 @@
-use std::ptr;
-use tm_rs::ffi as tm;
-use tm_rs::registry::RegistryApi;
+use tm_rs::log::LogApi;
+use tm_rs::{api, ffi as tm};
 
 #[no_mangle]
 #[allow(clippy::missing_safety_doc)]
 pub unsafe extern "C" fn tm_load_plugin(reg: *mut tm::tm_api_registry_api, load: bool) {
     assert!(!reg.is_null());
 
-    let tm_entity_api = reg.get(tm::TM_ENTITY_API_NAME) as *mut tm::tm_entity_api;
-    let tm_the_truth_api = reg.get(tm::TM_THE_TRUTH_API_NAME) as *mut tm::tm_the_truth_api;
-    let tm_graph_interpreter_api =
-        reg.get(tm::TM_GRAPH_INTERPRETER_API_NAME) as *mut tm::tm_graph_interpreter_api;
-    let tm_graph_component_api =
-        reg.get(tm::TM_GRAPH_COMPONENT_API_NAME) as *mut tm::tm_graph_component_api;
-    let tm_logger_api = reg.get(tm::TM_LOGGER_API_NAME) as *mut tm::tm_logger_api;
+    api::register::<LogApi>(reg);
 
-    (*tm_logger_api).print.unwrap()(
-        tm::tm_log_type_TM_LOG_TYPE_INFO,
-        b"Hej\0".as_ptr() as *const _,
-    );
+    api::get::<LogApi>().print_info("Hej");
 }
