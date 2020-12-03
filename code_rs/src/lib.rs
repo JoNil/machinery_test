d04d164b708c4670644fae07047ba3509a8a00b3
Jonathan Nilsson <jonathan@voysys.se>
2020-12-03 21:04:16 +0100

    Use safe interface

--- code_rs/src/lib.rs
+++ code_rs/src/lib.rs
@@ -14,18 +14,15 @@ use tm_rs::{entity::EntityApi, log::LogApi};
 
 static COMPONENT_NAME: &str = "light_distance_component";
 
-fn engine_update(update_set: &mut tm_engine_update_set_t) {
+fn engine_update(components: ComponentsIterator<(Write<LightComponent>, Read<GraphComponent>)>) {
     let log = api::get::<LogApi>();
-
-    let components =
-        ComponentsIterator::<(Write<LightComponent>, Read<GraphComponent>)>::new(update_set);
 
     for (light, graph) in components {
         let mut graph = api::with_ctx::<GraphInterpreterApi>(graph.gr);
 
         if let Some(distance_to_wall) = graph.read_variable_f32("Dist") {
             let hue = (f32::sin(0.4 * distance_to_wall) + 1.0) / 2.0;
-            log.info(&format!("H: {}", hue));
+            log.info(&format!("WOHOO: {}", hue));
             let color = Rgb::from_color(&Hsv::new((hue * 360.0) as f64, 1.0, 0.6));
             light.color_rgb = tm_vec3_t {
                 x: color.r as f32 / 255.0,
@@ -49,7 +46,7 @@ tm_plugin!(|reg: &mut RegistryApi| {
     add_or_remove_entity_simulation!(
         reg,
         fn register_light_engine(entity_api: &mut EntityApiInstance) {
-            entity_api.register_engine::<(Write<LightComponent>, Read<GraphComponent>)>(
+            entity_api.register_engine(
                 "Light Distance Component",
                 engine_update,
                 Some(engine_filter),
